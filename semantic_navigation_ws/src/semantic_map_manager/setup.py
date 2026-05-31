6e9c3c76de9392219ee73b5a5a62eee8de85b40d
rreisr00 <rreisr00@estudiantes.unileon.es>
2026-06-01 00:07:00 +0200

    Update semantic_map_manager package manifests and build deps

--- semantic_navigation_ws/src/semantic_map_manager/setup.py
+++ semantic_navigation_ws/src/semantic_map_manager/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "semantic_map_manager"
@@ -9,6 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
         (f"share/{package_name}", ["package.xml"]),
+        (f"share/{package_name}/config", glob("config/*.yaml")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -23,6 +26,10 @@ setup(
             "waypoint_capture = semantic_map_manager.waypoint_capture:main",
             "semantic_navigator = semantic_map_manager.semantic_navigator:main",
             "test_integration = semantic_map_manager.test_integration:main",
+            "visual_encoder = semantic_map_manager.visual_encoder_node:main",
+            "kg_manager = semantic_map_manager.kg_manager_node:main",
+            "semantic_orchestrator = semantic_map_manager.semantic_orchestrator_node:main",
+            "evaluation_node = semantic_map_manager.evaluation_node:main",
         ],
     },
 )
